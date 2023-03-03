54ec692638390eac7583bc1275b344468d2b5be7
dxcx <dxcxpolygon@gmail.com>
2023-03-03 21:45:33 +0100

    tradewindow init

---
+++ app/components/tradeWindow.tsx
@@ -0,0 +1,11 @@
+export interface TradeWindowProps {
+    currency: number;
+}
+const TradeWindow: React.FC<TradeWindowProps> = ({ currency }) => {
+    return (
+        <div className="tradewindow">
+            
+        </div>
+    )
+}
+export default TradeWindow;
