bd3998fadcbeb9a6a83790110e59ea509225d384
dxcx <dxcxpolygon@gmail.com>
2023-03-07 14:16:47 +0100

    basic UI for trading

--- app/components/tradeWindow.tsx
+++ app/components/tradeWindow.tsx
@@ -1,10 +1,47 @@
+import tradeLogo from "~/assets/tradelogo.png"
+import vividlf from "~/assets/vividlf.png"
+import DivCardGenerator from "./divCardGenerator";
+
+
 export interface TradeWindowProps {
     currency: number;
+    divcards: any;
 }
-const TradeWindow: React.FC<TradeWindowProps> = ({ currency }) => {
+
+
+const TradeWindow: React.FC<TradeWindowProps> = ({ currency, divcards }) => {
     return (
         <div className="tradewindow">
-            
+            <div className="header">
+                <div className="logo">
+                    <img src={tradeLogo} />
+                </div>
+                <div className="currency">
+                    {currency} Div
+                </div>
+            </div>
+            <div className="searchbar">
+                <div className="buttons">
+                    <button>Search</button> <button>Reset</button>
+                </div>
+                <div className="inputField">
+                    <input></input>
+                </div>
+            </div>
+            <div className="itemSelection">
+                <div className="lifeforce">
+                    <img src={vividlf} />
+                    Vivid Crystallised Lifeforce
+                </div>
+                <div className="divcards">
+                    <div className="options"></div>
+                    <div className="cards">
+                        {divcards.map(divcard => (
+                            <DivCardGenerator divcard={divcard} />
+                        ))}
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,23 @@
+import { json, LinksFunction } from "@remix-run/node";
+import TradeWindow from "~/components/tradeWindow";
+import stylesUrl from "~/style/index.css";
+import divCards from '~/data/divcards.json';
+import { useLoaderData } from "@remix-run/react";
+
+export const links: LinksFunction = () => {
+  return [{ rel: "stylesheet", href: stylesUrl }];
+};
+
+export const loader = async () => {
+  return json(divCards);
+}
+
 export default function Index() {
+  const divcards = useLoaderData<typeof loader>();
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
       <h1>Welcome to Gamba Game</h1>
+      <TradeWindow currency={0} divcards={divcards} />
     </div>
   );
 }
