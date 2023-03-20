18294c92d1bc392fcf023e3a5fa16605d29176c6
dxcx <dxcxpolygon@gmail.com>
2023-03-20 16:00:25 +0100

    inventory styling

--- app/components/inventoryWindow.tsx
+++ app/components/inventoryWindow.tsx
@@ -1,11 +1,18 @@
 import Inventory from "./Inventory/Inventory";
+import divineicon from "~/assets/divineorb.png";
 
 const InventoryWindow = () => {
 	return (
 		<div className="inventoryWindow">
 			<div className="frame"></div>
+			<div className="text">
+				Inventory
+			</div>
 			<div className="inventorygrid">
 				<Inventory />
+			</div>
+			<div className="currency">
+				0 <img src={divineicon} alt="divine" title="divine" />
 			</div>
 			<div className="bottomframe"></div>
 		</div>
