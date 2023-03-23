4feddeafd5c6b1ba3bae36230c0e19338468efd5
dxcx <dxcxpolygon@gmail.com>
2023-03-23 22:20:34 +0100

    styling contextmenu

--- app/components/contextMenu.tsx
+++ app/components/contextMenu.tsx
@@ -2,11 +2,14 @@ export interface ContextMenuProps { }
 const ContextMenu: React.FC<ContextMenuProps> = ({ }) => {
     return (
         <div className="itemContextMenu">
-            <div className="closeButton"></div>
-            <div className="topWrapper"></div>
-            <div className="buttons">
-                <button>Turn Item In</button>
-                <button>Sell Item</button>
+            <div className="closeButton" onClick={()=>console.log("close window")}>
+            </div>
+            <div className="options">
+                <div className="topWrapper"></div>
+                <div className="buttons">
+                    <button>Turn Item In</button>
+                    <button>Sell Item</button>
+                </div>
             </div>
             <div className="bottomWrapper"></div>
         </div>)
--- app/routes/sandbox.tsx
+++ app/routes/sandbox.tsx
@@ -1,23 +1,20 @@
+import { LinksFunction } from "@remix-run/node";
 import { useAtom } from "jotai";
+import ContextMenu from "~/components/contextMenu";
 import Inventory from "~/components/Inventory/Inventory";
 import MouseFollower from "~/components/MouseFollower/MouseFollower";
 import UnstackWindow from "~/components/UnstackWindow/UnstackWindow";
+import stylesUrl from "~/style/index.css";
 import { unStackVar } from ".";
-
+export const links: LinksFunction = () => {
+	return [{ rel: "stylesheet", href: stylesUrl }];
+};
 export default function Sandbox() {
 	const [spawnUnstack, SetSpawnUnstack] = useAtom(unStackVar);
 
 	return (
-		<div
-			className="select-none"
-			onClick={() => {
-				if (spawnUnstack) {
-					SetSpawnUnstack(false);
-				}
-			}}>
-			{spawnUnstack ? <UnstackWindow /> : null}
-			<MouseFollower />
-			<Inventory />
+		<div>
+			<ContextMenu></ContextMenu>
 		</div>
 	);
 }
